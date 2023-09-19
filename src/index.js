aaf823971a6df40de586fdbfa2b2823b8558617f
cybergrazi <graziellecvergilio@gmail.com>
2023-09-19 16:40:44 -0300

    Adicionado intermediario

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { listagemCarros, obterCarro } = require("./controladores/carros"); // importação
+const { validarSenha } = require("./intermediarios");
 
 const app = express();
+
+app.use(validarSenha);
 
 app.get("/carros", listagemCarros); // ROTA E CONTROLADOR AO LADO
 app.get("/carros/:id", obterCarro);
 
 app.listen(3000);
-
